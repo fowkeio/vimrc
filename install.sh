41af53f2b9d9560eae1c520e092bdbfefa500f57
fowkeio <guilteixeira@proton.com>
2022-11-11 10:02:01 -0500

    Adding a install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# APT UPDATE
+sudo apt update && \
+sudo apt upgrade -y && \
+sudo apt clean && \
+sudo apt autoclean && \
+sudo apt autoremove -y
+
+# APT INSTALL
+sudo apt install -y vim git curl wget build-essential cmake vim-nox python3-dev gcc g++ make ccls
+sudo apt clean && \
+sudo apt autoclean && \
+sudo apt autoremove -y
+
+# INSTALL VIM PLUG
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# GIT CLONE MY REPO AND FONT INSTALL
+cp -r fonts/ ~/.local/share/fonts
+fc-cache -fv
+
+# INSTALL .VIMRC
+cp .vimrc ~/.vimrc
+
+# VIM PLUGINTALL
+vim +PlugInstall +qall
+
+# YOUCOMPLETEME INSTALL
+python3  ~/.vim/plugged/YouCompleteMe/install.py --clangd-completer
+
+# COMPLETING INSTALL
+
+cp .ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
